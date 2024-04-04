8c72c3d040fc1f08b58600cd53d3ae8e3516fd75
anglusa <otavio.baroni@etec.sp.gov.br>
2024-04-04 11:34:22 -0300

    pagina alterada

--- Viota-Motors/Controllers/HomeController.cs
+++ Viota-Motors/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Viota_Motors.Models;
 
--- Viota-Motors/Models/Carro.cs
+++ Viota-Motors/Models/Carro.cs
@@ -1,13 +1,12 @@
 namespace Viota_Motors.Models
 {
-    public class Pokemon
+    public class Carro
     {
       public int Numero {get; set;}  
       public string Nome {get; set;}  
       public string Descricao {get; set;}  
-      public string Especie {get; set;}  
-      public List<string> Tipo {get; set;}  
-      public double Peso {get; set;}  
+      public string Cor {get; set;}  
+      public List<string> Tipo {get; set;}   
       public string Imagem {get; set;}  
     }
 }
